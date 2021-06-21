9713668407e708c4d556ee248dfac08e5fdc430e
slimane akdad <slimaneakdad@gmail.com>
2021-06-21 21:56:42 +0000

    Added draggable window

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,105 +10,141 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Commands bases website',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: DraggableArea(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class DraggableArea extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State createState() => _DraggableArea();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _DraggableArea extends State<DraggableArea> {
+  Offset _currentOffset = Offset.zero;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Positioned(
+            left: _currentOffset.dx,
+            top: _currentOffset.dy,
+            child: Draggable(
+              onDragEnd: (details) {
+                setState(() {
+                  _currentOffset = details.offset;
+                });
+              },
+              child: CMDWindow(),
+              feedback: Material(child: CMDWindow()),
+              childWhenDragging: Container(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class CMDWindow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 750,
+      height: 500,
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(0.7),
+                blurRadius: 15,
+                offset: Offset(0, 15),
+                spreadRadius: 10)
+          ]),
+      child: Column(
+        children: [
+          Container(
+            constraints: BoxConstraints.expand(height: 28),
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.4, 1],
+                    colors: [Colors.white, Colors.grey.withOpacity(0.1)])),
+            child: Row(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                        decoration: BoxDecoration(
+                            color: Colors.red, shape: BoxShape.circle),
+                        width: 15,
+                        height: 15),
+                    const SizedBox(width: 10),
+                    Container(
+                        decoration: BoxDecoration(
+                            color: Colors.yellow, shape: BoxShape.circle),
+                        width: 15,
+                        height: 15),
+                    const SizedBox(width: 10),
+                    Container(
+                        decoration: BoxDecoration(
+                            color: Colors.greenAccent, shape: BoxShape.circle),
+                        width: 15,
+                        height: 15),
+                    const SizedBox(width: 10),
+                  ],
+                ),
+                Expanded(
+                    child: Text(
+                  "roblef -- bash  -- 80x24",
+                  textAlign: TextAlign.center,
+                )),
+                Icon(Icons.exit_to_app)
+              ],
+            ),
+          ),
+          Expanded(
+              child: Container(
+            color: Colors.black,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Last login : Tue Jan 28 10:00:35 on console\n",
+                    style: TextStyle(color: const Color(0xff3aa832))),
+                Flexible(
+                  child: Container(
+                    child: TextField(
+                      autofocus: true,
+                      maxLines: 100,
+                      style:  TextStyle(color: const Color(0xff3aa832),height: 1),
+                      decoration: InputDecoration(
+                        isDense: true,
+                        contentPadding: EdgeInsets.zero,
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                        prefix: Text("Cmd-Window-Pro:~ User \$",
+                            style: TextStyle(color: const Color(0xff3aa832),height: 1)),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
+        ],
+      ),
     );
   }
 }
